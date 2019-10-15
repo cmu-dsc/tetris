5dc47f1988560836b4c1c0f53d5d06d782e9703b
Danny Darvish <darvish.osu@gmail.com>
2019-10-15 17:09:32 -0400

    plotting the gamestate for debugging should be implemented.

--- gamestate.py
+++ gamestate.py
@@ -1,4 +1,8 @@
 # This is the object which will serve as the interface to our data analysis code.
 class GameState:
     def __init__(self, playfield, active_piece, next_piece):
-        pass
+        self._playfield = playfield
+        self._active_piece = active_piece
+        self._next_piece = next_piece
+    def plot(self):
+        raise NotImplementedError
